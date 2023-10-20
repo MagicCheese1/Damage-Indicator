c4f56ebc66c71b3daabf323ac4c38fd514c99239
Kacper Płażewski <kacpik@mapik.eu>
2023-10-20 17:03:04 +0200

    Add server accessor for Minecraft version 1.20.2

--- src/main/java/com/github/magiccheese1/damageindicator/Main.java
+++ src/main/java/com/github/magiccheese1/damageindicator/Main.java
@@ -52,6 +52,9 @@ public class Main extends JavaPlugin {
             case "v1_20_R1":
                 packetManager = PacketManager1_20_R1.make();
                 break;
+            case "v1_20_R2":
+                packetManager = PacketManager1_20_R2.make();
+                break;
             default:
                 throw new RuntimeException("Failed to create version specific server accessor");
         }
